a6cef58e3cc51f40b8284db9d1a71d85f1e713c2
agilimam <agilimamalhafizi04@gmail.com>
2024-04-08 06:52:05 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("ini video windah");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini Audio windah");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar windah");
+});
